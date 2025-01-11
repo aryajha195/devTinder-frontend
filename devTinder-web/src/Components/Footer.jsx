804d89074411f71925c3b4edc436aac16212e555
Arya <aryajha195@gmail.com>
2025-01-11 22:09:06 +0530

    react-router-dom

---
+++ devTinder-web/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center bg-base-300 text-base-content p-4 bottom-0 fixed">
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+            </aside>
+        </footer>
+    )
+}
+
+export default Footer
